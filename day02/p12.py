f6ae8186f5ac53f4757c917980e751bab43eb7a0
Jiri Kozel <jirik@users.noreply.github.com>
2023-12-02 07:40:55 +0100

    day 2, part 2

--- day02/p12.py
+++ day02/p12.py
@@ -26,14 +26,26 @@ def reduce_line(prev, ln):
     return prev + game_id
 
 
+def reduce_line_part2(prev, ln):
+    game = ln.split(': ')[-1]
+    counts = {k: 0 for k in CUBES}
+    for rnd in game.split('; '):
+        for cube in rnd.split(', '):
+            cnt_str, color = cube.split(' ')
+            cnt = int(cnt_str)
+            counts[color] = max(cnt, counts[color])
+    return prev + functools.reduce(lambda x, y: x*y, counts.values())
+
+
 def main():
     for file_path in FILE_PATHS:
         with open(file_path) as f:
             lines = [ln.strip() for ln in f.readlines()]
 
         r1 = functools.reduce(reduce_line, lines, 0)
+        r2 = functools.reduce(reduce_line_part2, lines, 0)
 
-        print(file_path, r1)
+        print(file_path, r1, r2)
 
 
 if __name__ == "__main__":
