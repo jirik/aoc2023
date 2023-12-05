3cdc858bd55464d18a9ddf6ffc524bd6f509d858
Jiri Kozel <jirik@users.noreply.github.com>
2023-12-05 07:23:02 +0100

    day 5, part 2

--- day05/p12.py
+++ day05/p12.py
@@ -30,6 +30,27 @@ def find_location(seed, maps):
     return seed
 
 
+def find_location2(seeds, maps):
+    for mp in maps:
+        new_seeds = []
+        while seeds:
+            seed_s, seed_e = seeds.pop(0)
+            m = next(((drs, srs, rl) for drs, srs, rl in mp if seed_s <= srs + rl - 1 and seed_e >= srs), None)
+            if m is None:
+                new_seeds.append((seed_s, seed_e))
+            else:
+                drs, srs, rl = m
+                int_s = max(seed_s, srs)
+                int_e = min(seed_e, srs + rl - 1)
+                if int_s > seed_s:
+                    seeds.append((seed_s, int_s-1))
+                if int_e < seed_e:
+                    seeds.append((int_e + 1, seed_e))
+                new_seeds.append((int_s - srs + drs, int_e - srs + drs))
+        seeds = new_seeds
+    return new_seeds
+
+
 def main():
     for file_path in FILE_PATHS:
         with open(file_path) as f:
@@ -39,7 +60,9 @@ def main():
         maps = read_maps(lines[3:])
 
         r1 = min([find_location(s, maps) for s in seeds])
-        print(file_path, r1)
+        seeds = [(seeds[idx*2], seeds[idx*2] + seeds[idx*2+1] - 1) for idx in range(len(seeds)//2)]
+        r2 = min([loc for loc, _ in find_location2(seeds, maps)])
+        print(file_path, r1, r2)
 
 
 if __name__ == "__main__":
