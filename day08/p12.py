41618ea57e333c998587b4bb8e4a168d9e5e03cf
Jiri Kozel <jirik@users.noreply.github.com>
2023-12-08 13:28:32 +0100

    day 8, part 2

--- day08/p12.py
+++ day08/p12.py
@@ -2,6 +2,21 @@ from functools import reduce
 import re, os, math, sys
 
 
+def get_cycle(seq, lmap, rmap, cnode):
+    pth = []
+    step = 0
+    z_idxs = []
+    while not (len(z_idxs) == 1 and cnode and cnode[-1] == 'Z'):
+        pth.append(cnode)
+        if cnode[-1] == 'Z':
+            z_idxs.append(step)
+        move = seq[step % len(seq)]
+        mp = lmap if move == 'L' else rmap
+        cnode = mp[cnode]
+        step += 1
+    return z_idxs[0], len(pth) - z_idxs[0]
+
+
 def main():
     for file_path in sys.argv[1:]:
         with open(file_path) as f:
@@ -24,7 +39,13 @@ def main():
 
         r1 = step
 
-        print(file_path, r1)
+        cycles = [get_cycle(seq, lmap, rmap, n) for n in lmap if n[-1] == 'A']
+
+        # so we can ignore offset-related logic
+        assert all(offset == cycle_len for offset, cycle_len in cycles)
+
+        r2 = math.lcm(*[c[0] for c in cycles])
+        print(file_path, r1, r2)
 
 
 if __name__ == "__main__":
