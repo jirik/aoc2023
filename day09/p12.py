8c1323032b219517279e358c2204310a22d32f97
Jiri Kozel <jirik@users.noreply.github.com>
2023-12-09 06:39:41 +0100

    day 9, part 2

--- day09/p12.py
+++ day09/p12.py
@@ -2,7 +2,7 @@ from functools import reduce
 import re, os, math, sys
 
 
-def get_value(history):
+def get_values(history):
     diffs = [*history]
     seqs = [history]
     while not all(d == 0 for d in seqs[-1]):
@@ -12,7 +12,7 @@ def get_value(history):
             if idx > 0
         ]
         seqs.append(diffs)
-    return reduce(lambda p, s: s[-1] + p, seqs[::-1], 0)
+    return reduce(lambda p, s: s[0] - p, seqs[::-1], 0), reduce(lambda p, s: s[-1] + p, seqs[::-1], 0)
 
 
 def main():
@@ -22,9 +22,12 @@ def main():
 
         history = [[int(v) for v in ln.split(' ')] for ln in lines]
 
-        values = [get_value(ln) for ln in history]
-        r1 = sum(values)
-        print(file_path, r1)
+        tuples = [get_values(ln) for ln in history]
+        p_vals, n_vals = zip(*tuples)
+        r1 = sum(n_vals)
+        r2 = sum(p_vals)
+
+        print(file_path, r1, r2)
 
 
 if __name__ == "__main__":
