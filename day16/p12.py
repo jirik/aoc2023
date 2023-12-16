f61106326c64abd2c6415f7d0a989c6f272656e0
Jiri Kozel <jirik@users.noreply.github.com>
2023-12-16 07:11:10 +0100

    day 16, part 2

--- day16/p12.py
+++ day16/p12.py
@@ -39,6 +39,40 @@ def shift(prow, pcol, pdir):
     return r, c
 
 
+def energize(space, checked, prev_tuple):
+    qu = [prev_tuple]
+
+    while qu:
+        pr, pc, pdir = qu.pop(0)
+        ri, ci = shift(pr, pc, pdir)
+        if not (0 <= ri < len(space) and 0 <= ci < len(space)):
+            continue
+
+        coord = (ri, ci)
+        if coord not in checked:
+            checked[coord] = []
+        if pdir in checked[coord]:
+            continue
+        else:
+            checked[coord].append(pdir)
+
+        c = space[ri][ci]
+        if c == '.':
+            qu.append((ri, ci, pdir))
+        elif c == '-' and pdir in HDIRS or c == '|' and pdir in VDIRS:
+            qu.append((ri, ci, pdir))
+        elif c == '-':
+            qu += [(ri, ci, d) for d in HDIRS]
+        elif c == '|':
+            qu += [(ri, ci, d) for d in VDIRS]
+        elif c == '/':
+            qu.append((ri, ci, SLASH_MAP[pdir]))
+        elif c == '\\':
+            qu.append((ri, ci, BACKSLASH_MAP[pdir]))
+
+    return checked
+
+
 def main():
     for file_path in sys.argv[1:]:
         with open(file_path) as f:
@@ -46,39 +80,20 @@ def main():
         space = lines
 
         checked = {}
-        qu = [(0, -1, R)]
 
-        while qu:
-            pr, pc, pdir = qu.pop(0)
-            ri, ci = shift(pr, pc, pdir)
-            if not (0 <= ri < len(space) and 0 <= ci < len(space)):
-                continue
-
-            coord = (ri, ci)
-            if coord not in checked:
-                checked[coord] = []
-            if pdir in checked[coord]:
-                continue
-            else:
-                checked[coord].append(pdir)
-
-            c = space[ri][ci]
-            if c == '.':
-                qu.append((ri, ci, pdir))
-            elif c == '-' and pdir in HDIRS or c == '|' and pdir in VDIRS:
-                qu.append((ri, ci, pdir))
-            elif c == '-':
-                qu += [(ri, ci, d) for d in HDIRS]
-            elif c == '|':
-                qu += [(ri, ci, d) for d in VDIRS]
-            elif c == '/':
-                qu.append((ri, ci, SLASH_MAP[pdir]))
-            elif c == '\\':
-                qu.append((ri, ci, BACKSLASH_MAP[pdir]))
-
+        energize(space, checked, (0, -1, R))
         r1 = len(checked)
 
-        print(file_path, r1)
+        results = []
+        for ri in range(len(space)):
+            results.append(energize(space, {}, (ri, -1, R)))
+            results.append(energize(space, {}, (ri, len(space[0]), L)))
+        for ci in range(len(space[0])):
+            results.append(energize(space, {}, (-1, ci, D)))
+            results.append(energize(space, {}, (len(space), ci, U)))
+        r2 = max([len(c) for c in results])
+
+        print(file_path, r1, r2)
 
 
 if __name__ == "__main__":
