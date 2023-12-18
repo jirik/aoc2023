d5f906af7f3fbc3e78a665c1730afcc5f97a94f5
Jiri Kozel <jirik@users.noreply.github.com>
2023-12-18 07:21:58 +0100

    day 18, part 2

--- day18.py
+++ day18.py
@@ -7,10 +7,10 @@ from enum import IntEnum, Enum
 
 
 class Dir(Enum):
-    U = 'U'
+    R = 'R'
     D = 'D'
     L = 'L'
-    R = 'R'
+    U = 'U'
 
 
 def add_dir(row, col, direction, length):
@@ -26,26 +26,34 @@ def add_dir(row, col, direction, length):
         raise 'oops'
 
 
+def get_area(plan):
+    area = 0
+    border = 0
+    coord = (0, 0)
+    for drc, lng in plan:
+        r, c = coord
+        next_coord = add_dir(*coord, drc, lng)
+        nr, nc = next_coord
+        area += (r + nr) * (c - nc) / 2
+        border += abs(c - nc) if r == nr else abs(r - nr)
+        coord = next_coord
+    return round(area + border//2 + 1)
+
+
 def main():
     for file_path in sys.argv[1:]:
         with open(file_path) as f:
             lines = tuple(ln.strip() for ln in f.readlines())
 
         plan = [(Dir(parts[0]), int(parts[1])) for ln in lines if (parts := ln.split(' '))]
-        area = 0
-        border = 0
-        coord = (0, 0)
-        for drc, lng in plan:
-            r, c = coord
-            next_coord = add_dir(*coord, drc, lng)
-            nr, nc = next_coord
-            area += (r + nr) * (c - nc) / 2
-            border += abs(c - nc) if r == nr else abs(r - nr)
-            coord = next_coord
 
-        r1 = round(area + border//2 + 1)
+        r1 = get_area(plan)
 
-        print(file_path, r1)
+        dir_list = [e for e in Dir]
+        plan2 = [(dir_list[int(p[7])], int(p[2:7], 16)) for ln in lines if (p := ln.split(' ')[-1])]
+        r2 = get_area(plan2)
+
+        print(file_path, r1, r2)
 
 
 if __name__ == "__main__":
